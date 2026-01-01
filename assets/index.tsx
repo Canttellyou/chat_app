99e8421a2f9557453d19ba6d7af15415ab923cff
unknown <akbar123jason@gmail.com>
2026-01-01 22:12:37 +0000

    new changes

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,5 +1,5 @@
 import BgPattern from "./images/bgPattern.png";
-import SplashImage from "./images/splashImage.png";
+import SplashImage from "./images/splash-icon.png";
 import WelcomeImage from "./images/welcome.png";
 
 export { BgPattern, SplashImage, WelcomeImage };
